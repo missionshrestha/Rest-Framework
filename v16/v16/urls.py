126b1e9e6e630d0ea9418a3edfa1a98fb4ab5314
Mission Shrestha <missionshrestha99@gmail.com>
2024-09-12 12:53:14 +0545

    ViewSet API Implemented

---
+++ v16/v16/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+# create Router Object
+router=DefaultRouter()
+
+# Register StudentViewSet with Router
+router.register('studentapi', views.StudentViewSet, basename='student')
+# No need to manage studentapi/<int> urls, because Router will manage it.
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
